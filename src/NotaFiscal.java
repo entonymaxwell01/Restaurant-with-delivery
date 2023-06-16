e7cb5e9b0e51f1fed4487a88a1ee135e4834f86e
Entony Maxwell <entonymaxwell01@gmail.com>
2023-06-16 11:46:05 -0300

    Criando Repositório

---
+++ src/NotaFiscal.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+class NotaFiscal {
+    private int ID;
+    private String horario;
+    private List<Pedido> pedidos;
+
+    public NotaFiscal(int ID, String horario) {
+        this.ID = ID;
+        this.horario = horario;
+        this.pedidos = new ArrayList<>();
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
+    public List<Pedido> getPedidos() {
+        return pedidos;
+    }
+
+    public void setPedidos(List<Pedido> pedidos) {
+        this.pedidos = pedidos;
+    }
+
+    public void gerarNotaFiscal(Pedido pedido) {
+        pedidos.add(pedido);
+    }
+}
