91d8a7c477b18fd50bdddb850dc562aa5a011416
Entony Maxwell <entonymaxwell01@gmail.com>
2023-06-21 18:07:07 -0300

    Implementando (view) interface de login

---
+++ src/delivery/system/Avaliacao.java
@@ -0,0 +1,46 @@
+package delivery.system;
+
+class Avaliacao {
+    private int ID;
+    private String comentario;
+    private String autor;
+    private int numEstrelas;
+
+    public Avaliacao(String comentario, String autor, int numEstrelas) {
+        this.comentario = comentario;
+        this.autor = autor;
+        this.numEstrelas = numEstrelas;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getNumEstrelas() {
+        return numEstrelas;
+    }
+
+    public void setNumEstrelas(int numEstrelas) {
+        this.numEstrelas = numEstrelas;
+    }
+}
---
+++ src/delivery/system/BalancoFinanceiro.java
@@ -0,0 +1,45 @@
+package delivery.system;
+
+class BalancoFinanceiro {
+    private float saldo;
+    private float entrada;
+    private float saida;
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+    public float getEntrada() {
+        return entrada;
+    }
+
+    public void setEntrada(float entrada) {
+        this.entrada = entrada;
+    }
+
+    public float getSaida() {
+        return saida;
+    }
+
+    public void setSaida(float saida) {
+        this.saida = saida;
+    }
+
+    public void adicionarSaldo(float valor) {
+        saldo += valor;
+        entrada += valor;
+    }
+
+    public void removerSaldo(float valor) {
+        saldo -= valor;
+        saida += valor;
+    }
+
+    public float calcularBalanco() {
+        return entrada - saida;
+    }
+}
